8bd5d4fe7206a87dde54266371cd0c4d108ddefb
TobiasKlanert <tobias.klanert@t-online.de>
2025-03-19 14:15:15 +0100

    JSDoc events.js

--- js/events.js
+++ js/events.js
@@ -1,48 +1,65 @@
+/**
+ * Listens for changes in fullscreen mode and updates UI elements accordingly.
+ * @event fullscreenchange
+ * @listens document#fullscreenchange
+ */
 document.addEventListener("fullscreenchange", () => {
-    if (!document.fullscreenElement) {
-      fullscreenEnabled = false;
-      toggleBtnFullscreen(btnFullscreen);
-      canvas.classList.toggle("fullscreen");
-      arrangeButtons();
-    }
-  });
+  if (!document.fullscreenElement) {
+    fullscreenEnabled = false;
+    toggleBtnFullscreen(btnFullscreen);
+    canvas.classList.toggle("fullscreen");
+    arrangeButtons();
+  }
+});
 
-  window.addEventListener("keydown", (event) => {
-    switch (event.code) {
-      case "ArrowLeft":
-        keyboard.LEFT = true;
-        break;
-      case "ArrowRight":
-        keyboard.RIGHT = true;
-        break;
-      case "ArrowUp":
-        keyboard.UP = true;
-        break;
-      case "ArrowDown":
-        keyboard.DOWN = true;
-        break;
-      case "Space":
-        keyboard.SPACE = true;
-        break;
-      case "KeyD":
-        keyboard.D = true;
-        break;
-    }
-  });
-  
-  window.addEventListener("keyup", (event) => {
-    switch (event.code) {
-      case "ArrowLeft":
-        keyboard.LEFT = false;
-        break;
-      case "ArrowRight":
-        keyboard.RIGHT = false;
-        break;
-      case "ArrowUp":
-        keyboard.UP = false;
-        break;
-      case "ArrowDown":
-        keyboard.DOWN = false;
-        break;
-    }
-  });
+/**
+ * Handles keydown events to update the keyboard state.
+ * @event keydown
+ * @listens window#keydown
+ * @param {KeyboardEvent} event - The event object containing the key pressed.
+ */
+window.addEventListener("keydown", (event) => {
+  switch (event.code) {
+    case "ArrowLeft":
+      keyboard.LEFT = true;
+      break;
+    case "ArrowRight":
+      keyboard.RIGHT = true;
+      break;
+    case "ArrowUp":
+      keyboard.UP = true;
+      break;
+    case "ArrowDown":
+      keyboard.DOWN = true;
+      break;
+    case "Space":
+      keyboard.SPACE = true;
+      break;
+    case "KeyD":
+      keyboard.D = true;
+      break;
+  }
+});
+
+/**
+ * Handles keyup events to update the keyboard state.
+ * @event keyup
+ * @listens window#keyup
+ * @param {KeyboardEvent} event - The event object containing the key released.
+ */
+window.addEventListener("keyup", (event) => {
+  switch (event.code) {
+    case "ArrowLeft":
+      keyboard.LEFT = false;
+      break;
+    case "ArrowRight":
+      keyboard.RIGHT = false;
+      break;
+    case "ArrowUp":
+      keyboard.UP = false;
+      break;
+    case "ArrowDown":
+      keyboard.DOWN = false;
+      break;
+  }
+});
