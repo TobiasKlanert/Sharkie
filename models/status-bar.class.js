b24654b4303a6ac6915ff9f24181debcf4d1012e
TobiasKlanert <tobias.klanert@t-online.de>
2025-03-19 15:53:56 +0100

    JSDoc statusbar.class.js

--- models/status-bar.class.js
+++ models/status-bar.class.js
@@ -1,4 +1,13 @@
+/**
+ * Represents a status bar in the game.
+ * The status bar displays the player's progress for bottles, life, or coins.
+ * This class extends the `DrawableObject` class.
+ */
 class StatusBar extends DrawableObject {
+  /**
+   * An array of image paths for the bottles status bar.
+   * @type {string[]}
+   */
   IMAGES_BOTTLES = [
     "graphics/4. Marcadores/green/poisoned bubbles/0_ copia 2.png",
     "graphics/4. Marcadores/green/poisoned bubbles/20_ copia 3.png",
@@ -8,6 +17,10 @@ class StatusBar extends DrawableObject {
     "graphics/4. Marcadores/green/poisoned bubbles/100_ copia 3.png",
   ];
 
+  /**
+   * An array of image paths for the life status bar.
+   * @type {string[]}
+   */
   IMAGES_LIFE = [
     "graphics/4. Marcadores/green/Life/0_  copia 3.png",
     "graphics/4. Marcadores/green/Life/20_ copia 4.png",
@@ -17,6 +30,10 @@ class StatusBar extends DrawableObject {
     "graphics/4. Marcadores/green/Life/100_  copia 2.png",
   ];
 
+  /**
+   * An array of image paths for the coins status bar.
+   * @type {string[]}
+   */
   IMAGES_COINS = [
     "graphics/4. Marcadores/green/Coin/0_  copia 4.png",
     "graphics/4. Marcadores/green/Coin/20_  copia 2.png",
@@ -26,6 +43,14 @@ class StatusBar extends DrawableObject {
     "graphics/4. Marcadores/green/Coin/100_ copia 4.png",
   ];
 
+  /**
+   * The offset values for collision detection.
+   * @type {Object}
+   * @property {number} top - The top offset.
+   * @property {number} left - The left offset.
+   * @property {number} right - The right offset.
+   * @property {number} bottom - The bottom offset.
+   */
   offset = {
     top: 0,
     left: 0,
@@ -33,8 +58,19 @@ class StatusBar extends DrawableObject {
     bottom: 0,
   };
 
+  /**
+   * The current percentage value displayed by the status bar.
+   * @type {number}
+   */
   percentage = 100;
 
+  /**
+   * Creates a new status bar object.
+   * @param {string} type - The type of status bar ("bottles", "life", or "coins").
+   * @param {number} x - The x-coordinate of the status bar.
+   * @param {number} y - The y-coordinate of the status bar.
+   * @param {number} percentage - The initial percentage value for the status bar.
+   */
   constructor(type, x, y, percentage) {
     super();
     this.loadImages(this.IMAGES_BOTTLES);
@@ -48,6 +84,11 @@ class StatusBar extends DrawableObject {
     this.setPercentage(percentage, imageArr);
   }
 
+  /**
+   * Returns the appropriate image array based on the type of status bar.
+   * @param {string} type - The type of status bar ("bottles", "life", or "coins").
+   * @returns {string[]} The corresponding image array for the status bar.
+   */
   setImageArr(type) {
     switch (type) {
       case "bottles":
@@ -59,12 +100,21 @@ class StatusBar extends DrawableObject {
     }
   }
 
+  /**
+   * Sets the percentage value for the status bar and updates the displayed image.
+   * @param {number} percentage - The new percentage value.
+   * @param {string[]} imageArr - The image array corresponding to the status bar type.
+   */
   setPercentage(percentage, imageArr) {
     this.percentage = percentage;
     let imagePath = imageArr[this.resolveImageIndex()];
     this.img = this.imageCache[imagePath];
   }
 
+  /**
+   * Resolves the index of the image to display based on the current percentage value.
+   * @returns {number} The index of the image in the image array.
+   */
   resolveImageIndex() {
     if (this.percentage >= 100) {
       return 5;
